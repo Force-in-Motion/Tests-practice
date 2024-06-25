2dbff19ad8c6428e1ef81ae4edf3943ad767d48e
Sergey Gvozdenko <canzdao@gmail.com>
2024-06-25 14:08:36 +0300

    test

---
+++ hw_2/tests/tests.py
@@ -0,0 +1,32 @@
+import pytest
+from hw_2.src.main import average
+from hw_2.src.main import factorial
+
+data_test1 = [([], ValueError),
+             ([5], 5),
+             ([i for i in range(10000)], 4999.5),
+             ([3, 1, 4, 1, 5], 2.8),
+             ([-1, -3, -2, -4], -2.5),
+             ([2, -1, 3, -5, 0], -0.2),
+             ((bool, str, float, int, None), TypeError),
+             ([0, 0, 0, 0, 0, 0, 0], 0)]
+
+@pytest.mark.parametrize("value1, result1", data_test1)
+def test_average(value1, result1):
+
+    assert average(value1) == result1
+
+data_test2 = [(True, TypeError),
+             ('ananas', TypeError),
+             (127.8, TypeError),
+             (8, 40320),
+             (None, TypeError),
+             (0, 1),
+             (-78, ValueError),
+             (7, 5040)]
+
+
+@pytest.mark.parametrize('value2, result2', data_test2)
+def test_factorial(value2, result2):
+
+    assert factorial(value2) == result2
